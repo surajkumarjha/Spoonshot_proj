95ed9d04f025894d2640443c381fbde52e400226
surajkumarjha <surajjha013@gmail.com>
2021-03-06 15:11:20 +0530

    make it better

--- app.py
+++ app.py
@@ -30,5 +30,5 @@ def update(id):
     return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
